30f95871f4792b753f50469da2d0774c8f519f70
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2023-05-03 23:27:19 +0000

    Refactor of file and directory providing, removed updating from GitHub

--- fdw.sh
+++ fdw.sh
@@ -4,16 +4,18 @@
 # Default values
 interval=10
 command="echo Detected change"
+filesAndFolders=( "." )
 
 # Function that displays the help
 displayHelp() {
-    echo "Usage: fdw.sh [options...] [files or folders to watch...]"
+    echo "Usage: fdw.sh [options...]"
     echo "Watch files or/and directories and execute a command when they change."
     echo ""
     echo "-i, --interval:               Interval between watches"
     echo "-b, --background:             Run command in background"
     echo "-c, --command:                Command to execute"
-    echo "-u, --update:                 Update script from GitHub repository"
+    echo "-f, --file"
+    echo "-d, --directory:              Files or directories to watch, stop with --"
     echo "-h, --help:                   Show this help"
 }
 
@@ -24,7 +26,6 @@ if [[ $# -eq 0 ]]; then
 fi
 
 # Parsing command line arguments
-watched_files_and_folders=()
 while [[ $# -gt 0 ]]; do
     case $1 in
 
@@ -54,10 +55,17 @@ while [[ $# -gt 0 ]]; do
         shift 1
     ;;
 
-    # Update script from GitHub repository
-    -u|--update)
-        curl https://raw.githubusercontent.com/michalpokusa/file-directory-watcher/main/fdw.sh > $0
-        exit 0
+    # Files or directories to watch
+    -f|--file|-d|--directory)
+        shift 1
+        filesAndFolders=()
+        for fileOrFolder in "$@"; do
+            shift 1
+            if [[ "$fileOrFolder" == "--" ]]; then
+                break
+            fi
+            filesAndFolders+=( "$fileOrFolder" )
+        done
     ;;
 
     # Show this help
@@ -67,44 +75,45 @@ while [[ $# -gt 0 ]]; do
     ;;
 
     # Unknown option
-    -*|--*)
+    -*|--*|*)
         echo Unknown option $1
         exit 1
     ;;
-
-    # File or directory to watch
-    *)
-        watched_files_and_folders+=("$1")
-        shift 1
-    ;;
     esac
 done
-set -- "${watched_files_and_folders[@]}"
 
 
 # Function that returns the current time in pre-defined format
 currentTime() {
-    echo $(date +'%Y-%m-%d %H:%M:%S.%N%:z')
+    echo $(date +'%Y-%m-%d %H:%M:%S')
 }
 
 # Function that returns the current state of the watched files and directories
 currentDirectoryState() {
-    echo $(ls -lu --almost-all --recursive --full-time $@ 2>/dev/null)
+    echo $(ls -lu --almost-all --recursive --full-time "${filesAndFolders[@]}" 2>/dev/null)
+}
+
+verboseFilesAndFolders() {
+    output=""
+    for name in "${filesAndFolders[@]}"; do
+        output+="\"$name\", "
+    done
+    echo "${output::-2}"
 }
 
 
-echo "[$(currentTime)] Watching ${watched_files_and_folders[@]} every ${interval}s..."
+echo "[$(currentTime)] Watching $(verboseFilesAndFolders) every ${interval}s, on change running: $command"
 
-last_time_state="$(currentDirectoryState ${watched_files_and_folders[@]})"
+lastTimeState="$(currentDirectoryState)"
 
 while sleep $interval; do
 
-    current_state="$(currentDirectoryState ${watched_files_and_folders[@]})"
+    currentState="$(currentDirectoryState)"
 
     # Checking if the state of the file/directory has changed
-    if ([[ $last_time_state != $current_state ]] )
+    if ([[ $lastTimeState != $currentState ]] )
     then
-        last_time_state="$current_state"
+        lastTimeState="$currentState"
 
         # Executing the command in background if specified, otherwise executing it in foreground
         if [[ $background ]]
@@ -116,7 +125,7 @@ while sleep $interval; do
             eval $command
         fi
 
-        echo "[$(currentTime)] Back to watching ${type^} $watched_file_or_folder"
+        echo "[$(currentTime)] Back to watching $(verboseFilesAndFolders)"
     fi
 done
 }
